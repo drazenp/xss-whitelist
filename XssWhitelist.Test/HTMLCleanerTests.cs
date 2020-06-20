eda75e4bc21fe51bba1bb0c675c16edb1cb265ee
Drazen Pupovac <drazenp86@gmail.com>
2020-06-20 17:03:43 +0100

    Remove not allowed pre-defined nodes

--- XssWhitelist.Test/HTMLCleanerTests.cs
+++ XssWhitelist.Test/HTMLCleanerTests.cs
@@ -11,13 +11,65 @@ namespace XssWhitelist.Test
         public void Clean_EmptyDocument_Pass()
         {
             // Arrange
-            var document = new XDocument();
+            var document = XDocument.Parse("<div></div>");
 
             // Act
             new HTMLCleaner().Clean(document);
 
             // Assert
-            Assert.That(document.Descendants().Any(), Is.False);
+            Assert.That(document.Root.ToString(), Is.EqualTo("<div></div>"));
+        }
+
+        [Test]
+        public void Clean_DocumentWithOnlyText_DoesNotChange()
+        {
+            // Arrange
+            var document = XDocument.Parse("<div>test</div>");
+
+            // Act
+            new HTMLCleaner().Clean(document);
+
+            // Assert
+            Assert.That(document.Root.ToString(), Is.EqualTo("<div>test</div>"));
+        }
+
+        [Test]
+        public void Clean_WithNotAllowedNode_RemovesNotAllowedNode()
+        {
+            // Arrange
+            var document = XDocument.Parse("<div><test></test></div>");
+
+            // Act
+            new HTMLCleaner().Clean(document);
+
+            // Assert
+            Assert.That(document.Root.ToString(), Is.EqualTo("<div />"));
+        }
+
+        [Test]
+        public void Clean_WithNestedNotAllowedNode_RemovesNotAllowedNodes()
+        {
+            // Arrange
+            var document = XDocument.Parse("<div><test><test></test></test></div>");
+
+            // Act
+            new HTMLCleaner().Clean(document);
+
+            // Assert
+            Assert.That(document.Root.ToString(), Is.EqualTo("<div />"));
+        }
+
+        [Test]
+        public void Clean_WithNestedAllowedNodeInsideNotAllowed_RemovesNotAllowedNodeAndSubNodes()
+        {
+            // Arrange
+            var document = XDocument.Parse("<div><test><div></div></test></div>");
+
+            // Act
+            new HTMLCleaner().Clean(document);
+
+            // Assert
+            Assert.That(document.Root.ToString(), Is.EqualTo("<div />"));
         }
     }
 }
--- XssWhitelist/HTMLCleaner.cs
+++ XssWhitelist/HTMLCleaner.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace XssWhitelist
 {
     public class HTMLCleaner : IHTMLCleaner
     {
+        private HashSet<string> _allowedNodes = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { "h1", "div" };
+
         public void Clean(XDocument xml)
         {
-
+            xml.Root.Descendants().Where(x => !_allowedNodes.Contains(x.Name.LocalName)).Remove();
         }
     }
 }
